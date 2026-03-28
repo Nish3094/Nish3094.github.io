1e85a67fb501b8c5ed80be2d3cd368e039c60e6a
Nish3094 <39150263+Nish3094@users.noreply.github.com>
2026-03-29 01:27:37 +0530

    Update Firebase imports and add authentication

--- firebase.js
+++ firebase.js
@@ -1,23 +1,17 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth, GithubAuthProvider } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCREdxSPT6BYh7TXISSgYPXka7fQ2xD8-8",
   authDomain: "nishverse.firebaseapp.com",
   projectId: "nishverse",
   storageBucket: "nishverse.firebasestorage.app",
   messagingSenderId: "522510581544",
-  appId: "1:522510581544:web:442867fbdea205df449a1d",
-  measurementId: "G-6802T1J0TB"
+  appId: "1:522510581544:web:442867fbdea205df449a1d"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth(app);
 const provider = new GithubAuthProvider();
+
 export { auth, provider };
