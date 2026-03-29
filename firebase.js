a669bf3d0cdc6ac329fc43d8c87f668428d1f762
Nish3094 <39150263+Nish3094@users.noreply.github.com>
2026-03-29 10:20:01 +0530

    Fix formatting and remove extra newlines in firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,11 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth, GithubAuthProvider } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
- 
+
 const firebaseConfig = {
   apiKey: "AIzaSyCREdxSPT6BYh7TXISSgYPXka7fQ2xD8-8",
   authDomain: "nishverse.firebaseapp.com"
 };
- 
+
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const provider = new GithubAuthProvider();
